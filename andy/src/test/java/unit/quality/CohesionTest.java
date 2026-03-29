cb05e089446974cb016725014c00a45f8694976c
Afonso Vicente Nobre <a.m.vicentenobre@student.tudelft.nl>
2026-03-29 15:08:41 +0200

    Making tests run

--- andy/src/test/java/unit/quality/CohesionTest.java
+++ andy/src/test/java/unit/quality/CohesionTest.java
@@ -12,96 +12,88 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CohesionTest {
 
-//    @Test
-//    public void testTriggersOneMetaTestTest() {
-//        QualityResult qualityResult = new QualityResult(1);
-//        qualityResult.setUnitTests(Map.of("testID", "test"));
-//
-//        TestFailureInfo failure = new TestFailureInfo("test", "error message");
-//        MetaTestReport metaTestReport = new MetaTestReport(1, 0, 1, List.of(failure));
-//        metaTestReport.setName("meta-test");
-//        qualityResult.considerMetaTest(metaTestReport);
-//
-//        assertEquals(1, qualityResult.countCohesiveTests());
-//    }
-//
-//    @Test
-//    public void testTriggersNoMetaTestTest() {
-//        QualityResult qualityResult = new QualityResult(1);
-//        qualityResult.setUnitTests(Map.of("testID", "test"));
-//
-//        MetaTestReport metaTestReport = new MetaTestReport(1, 1, 1, List.of());
-//        metaTestReport.setName("meta-test");
-//        qualityResult.considerMetaTest(metaTestReport);
-//
-//        assertEquals(0, qualityResult.countCohesiveTests());
-//    }
-//
-//    @Test
-//    public void sameTestTriggersTwoMetaTestsTest() {
-//        QualityResult qualityResult = new QualityResult(1);
-//        qualityResult.setUnitTests(Map.of("testID", "test"));
-//
-//        TestFailureInfo failure1 = new TestFailureInfo("test", "error message");
-//        TestFailureInfo failure2 = new TestFailureInfo("test", "some other error message");
-//        MetaTestReport metaTestReport1 = new MetaTestReport(1, 1, 1, List.of(failure1));
-//        metaTestReport1.setName("meta-test1");
-//        MetaTestReport metaTestReport2 = new MetaTestReport(1, 1, 1, List.of(failure2));
-//        metaTestReport2.setName("meta-test2");
-//        qualityResult.considerMetaTest(metaTestReport1);
-//        qualityResult.considerMetaTest(metaTestReport2);
-//
-//        assertEquals(0, qualityResult.countCohesiveTests());
-//    }
-//
-//    @Test
-//    public void differentTestsTriggerDifferentMetaTestsTest() {
-//        QualityResult qualityResult = new QualityResult(2);
-//        qualityResult.setUnitTests(Map.of("test1ID", "test1", "test2ID", "test2"));
-//
-//        TestFailureInfo failure1 = new TestFailureInfo("test1", "error message");
-//        TestFailureInfo failure2 = new TestFailureInfo("test2", "some other error message");
-//        MetaTestReport metaTestReport1 = new MetaTestReport(2, 1, 2, List.of(failure1));
-//        metaTestReport1.setName("meta-test1");
-//        MetaTestReport metaTestReport2 = new MetaTestReport(2, 1, 2, List.of(failure2));
-//        metaTestReport2.setName("meta-test2");
-//        qualityResult.considerMetaTest(metaTestReport1);
-//        qualityResult.considerMetaTest(metaTestReport2);
-//
-//        assertEquals(2, qualityResult.countCohesiveTests());
-//    }
-//
-//    @Test
-//    public void differentTestsTriggerSameMetaTestsTest() {
-//        QualityResult qualityResult = new QualityResult(2);
-//        qualityResult.setUnitTests(Map.of("test1ID", "test1", "test2ID", "test2"));
-//
-//        TestFailureInfo failure1 = new TestFailureInfo("test1", "error message");
-//        TestFailureInfo failure2 = new TestFailureInfo("test2", "some other error message");
-//        MetaTestReport metaTestReport = new MetaTestReport(2, 0, 2, List.of(failure1, failure2));
-//        metaTestReport.setName("meta-test");
-//        qualityResult.considerMetaTest(metaTestReport);
-//
-//        assertEquals(2, qualityResult.countCohesiveTests());
-//    }
-//
-//    @Test
-//    public void noTestsTest() {
-//        QualityResult qualityResult = new QualityResult(0);
-//        qualityResult.setUnitTests(Map.of());
-//
-//        MetaTestReport metaTestReport = new MetaTestReport(0, 0, 0, List.of());
-//        metaTestReport.setName("meta-test");
-//        qualityResult.considerMetaTest(metaTestReport);
-//
-//        assertEquals(0, qualityResult.countCohesiveTests());
-//    }
-//
-//    @Test
-//    public void noMetaTestsTest() {
-//        QualityResult qualityResult = new QualityResult(1);
-//        qualityResult.setUnitTests(Map.of("testID", "test"));
-//
-//        assertEquals(0, qualityResult.countCohesiveTests());
-//    }
+    @Test
+    public void testTriggersOneMetaTestTest() {
+        QualityResult qualityResult = new QualityResult(1);
+        qualityResult.setUnitTests(Map.of("testID", "test"));
+
+        TestFailureInfo failure = new TestFailureInfo("test", "error message");
+        MetaTestReport metaTestReport = new MetaTestReport(1, 0, 1, List.of(failure));
+        qualityResult.considerMetaTest("meta-test", metaTestReport);
+
+        assertEquals(1, qualityResult.countCohesiveTests());
+    }
+
+    @Test
+    public void testTriggersNoMetaTestTest() {
+        QualityResult qualityResult = new QualityResult(1);
+        qualityResult.setUnitTests(Map.of("testID", "test"));
+
+        MetaTestReport metaTestReport = new MetaTestReport(1, 1, 1, List.of());
+        qualityResult.considerMetaTest("meta-test", metaTestReport);
+
+        assertEquals(0, qualityResult.countCohesiveTests());
+    }
+
+    @Test
+    public void sameTestTriggersTwoMetaTestsTest() {
+        QualityResult qualityResult = new QualityResult(1);
+        qualityResult.setUnitTests(Map.of("testID", "test"));
+
+        TestFailureInfo failure1 = new TestFailureInfo("test", "error message");
+        TestFailureInfo failure2 = new TestFailureInfo("test", "some other error message");
+        MetaTestReport metaTestReport1 = new MetaTestReport(1, 1, 1, List.of(failure1));
+        MetaTestReport metaTestReport2 = new MetaTestReport(1, 1, 1, List.of(failure2));
+        qualityResult.considerMetaTest("meta-test1", metaTestReport1);
+        qualityResult.considerMetaTest("meta-test2", metaTestReport2);
+
+        assertEquals(0, qualityResult.countCohesiveTests());
+    }
+
+    @Test
+    public void differentTestsTriggerDifferentMetaTestsTest() {
+        QualityResult qualityResult = new QualityResult(2);
+        qualityResult.setUnitTests(Map.of("test1ID", "test1", "test2ID", "test2"));
+
+        TestFailureInfo failure1 = new TestFailureInfo("test1", "error message");
+        TestFailureInfo failure2 = new TestFailureInfo("test2", "some other error message");
+        MetaTestReport metaTestReport1 = new MetaTestReport(2, 1, 2, List.of(failure1));
+        MetaTestReport metaTestReport2 = new MetaTestReport(2, 1, 2, List.of(failure2));
+        qualityResult.considerMetaTest("meta-test1", metaTestReport1);
+        qualityResult.considerMetaTest("meta-test2", metaTestReport2);
+
+        assertEquals(2, qualityResult.countCohesiveTests());
+    }
+
+    @Test
+    public void differentTestsTriggerSameMetaTestsTest() {
+        QualityResult qualityResult = new QualityResult(2);
+        qualityResult.setUnitTests(Map.of("test1ID", "test1", "test2ID", "test2"));
+
+        TestFailureInfo failure1 = new TestFailureInfo("test1", "error message");
+        TestFailureInfo failure2 = new TestFailureInfo("test2", "some other error message");
+        MetaTestReport metaTestReport = new MetaTestReport(2, 0, 2, List.of(failure1, failure2));
+        qualityResult.considerMetaTest("meta-test", metaTestReport);
+
+        assertEquals(2, qualityResult.countCohesiveTests());
+    }
+
+    @Test
+    public void noTestsTest() {
+        QualityResult qualityResult = new QualityResult(0);
+        qualityResult.setUnitTests(Map.of());
+
+        MetaTestReport metaTestReport = new MetaTestReport(0, 0, 0, List.of());
+        qualityResult.considerMetaTest("meta-test", metaTestReport);
+
+        assertEquals(0, qualityResult.countCohesiveTests());
+    }
+
+    @Test
+    public void noMetaTestsTest() {
+        QualityResult qualityResult = new QualityResult(1);
+        qualityResult.setUnitTests(Map.of("testID", "test"));
+
+        assertEquals(0, qualityResult.countCohesiveTests());
+    }
 }
--- andy/src/test/java/unit/quality/ContributionTest.java
+++ andy/src/test/java/unit/quality/ContributionTest.java
@@ -13,83 +13,80 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ContributionTest {
 
-//    @Test
-//    public void testNoTestsContributeWhenAllMapsEmpty() {
-//        QualityResult qualityResult = new QualityResult(3);
-//        qualityResult.setUnitTests(Map.of("id1", "test1", "id2", "test2", "id3", "test3"));
-//        // no meta tests, coverage, or mutations set
-//        assertEquals(0, qualityResult.countContributingTests());
-//    }
-//
-//    @Test
-//    public void testContributesViaMetaTestOnly() {
-//        QualityResult qualityResult = new QualityResult(1);
-//        qualityResult.setUnitTests(Map.of("id1", "test1"));
-//
-//        TestFailureInfo failure = new TestFailureInfo("test1", "error");
-//        MetaTestReport report = new MetaTestReport(1, 1, 1, List.of(failure));
-//        report.setName("meta-test1");
-//        qualityResult.considerMetaTest(report);
-//
-//        assertEquals(1, qualityResult.countContributingTests());
-//    }
-//
-//    @Test
-//    public void testContributesViaCoverageOnly() {
-//        QualityResult qualityResult = new QualityResult(1);
-//        qualityResult.setUnitTests(Map.of("id1", "test1"));
-//        qualityResult.setCoveragePerTest(Map.of("test1", Set.of(10, 11, 12)));
-//
-//        assertEquals(1, qualityResult.countContributingTests());
-//    }
-//
-//    @Test
-//    public void testContributesViaMutationOnly() {
-//        QualityResult qualityResult = new QualityResult(1);
-//        qualityResult.setUnitTests(Map.of("id1", "test1"));
-//        qualityResult.setMutationsKilledPerTest(Map.of("test1", Set.of(1, 2)));
-//
-//        assertEquals(1, qualityResult.countContributingTests());
-//    }
-//
-//    @Test
-//    public void testSameTestContributesViaAllThree() {
-//        QualityResult qualityResult = new QualityResult(1);
-//        qualityResult.setUnitTests(Map.of("id1", "test1"));
-//
-//        TestFailureInfo failure = new TestFailureInfo("test1", "error");
-//        MetaTestReport report = new MetaTestReport(1, 1, 1, List.of(failure));
-//        report.setName("meta-test1");
-//        qualityResult.considerMetaTest(report);
-//        qualityResult.setCoveragePerTest(Map.of("test1", Set.of(10)));
-//        qualityResult.setMutationsKilledPerTest(Map.of("test1", Set.of(1)));
-//
-//        // still counts as 1 contributing test, not 3
-//        assertEquals(1, qualityResult.countContributingTests());
-//    }
-//
-//    @Test
-//    public void testMultipleTestsEachContributingDifferently() {
-//        QualityResult qualityResult = new QualityResult(3);
-//        qualityResult.setUnitTests(Map.of("id1", "test1", "id2", "test2", "id3", "test3"));
-//
-//        TestFailureInfo failure = new TestFailureInfo("test1", "error");
-//        MetaTestReport report = new MetaTestReport(1, 1, 1, List.of(failure));
-//        report.setName("meta-test1");
-//        qualityResult.considerMetaTest(report);
-//        qualityResult.setCoveragePerTest(Map.of("test2", Set.of(10)));
-//        qualityResult.setMutationsKilledPerTest(Map.of("test3", Set.of(1)));
-//
-//        assertEquals(3, qualityResult.countContributingTests());
-//    }
-//
-//    @Test
-//    public void testTestWithEmptySetsDoesNotContribute() {
-//        QualityResult qualityResult = new QualityResult(1);
-//        qualityResult.setUnitTests(Map.of("id1", "test1"));
-//        qualityResult.setCoveragePerTest(Map.of("test1", Set.of()));
-//        qualityResult.setMutationsKilledPerTest(Map.of("test1", Set.of()));
-//
-//        assertEquals(0, qualityResult.countContributingTests());
-//    }
+    @Test
+    public void testNoTestsContributeWhenAllMapsEmpty() {
+        QualityResult qualityResult = new QualityResult(3);
+        qualityResult.setUnitTests(Map.of("id1", "test1", "id2", "test2", "id3", "test3"));
+        // no meta tests, coverage, or mutations set
+        assertEquals(0, qualityResult.countContributingTests());
+    }
+
+    @Test
+    public void testContributesViaMetaTestOnly() {
+        QualityResult qualityResult = new QualityResult(1);
+        qualityResult.setUnitTests(Map.of("id1", "test1"));
+
+        TestFailureInfo failure = new TestFailureInfo("test1", "error");
+        MetaTestReport report = new MetaTestReport(1, 1, 1, List.of(failure));
+        qualityResult.considerMetaTest("meta-test1", report);
+
+        assertEquals(1, qualityResult.countContributingTests());
+    }
+
+    @Test
+    public void testContributesViaCoverageOnly() {
+        QualityResult qualityResult = new QualityResult(1);
+        qualityResult.setUnitTests(Map.of("id1", "test1"));
+        qualityResult.setCoveragePerTest(Map.of("test1", Set.of(10, 11, 12)));
+
+        assertEquals(1, qualityResult.countContributingTests());
+    }
+
+    @Test
+    public void testContributesViaMutationOnly() {
+        QualityResult qualityResult = new QualityResult(1);
+        qualityResult.setUnitTests(Map.of("id1", "test1"));
+        qualityResult.setMutationsKilledPerTest(Map.of("test1", Set.of(1, 2)));
+
+        assertEquals(1, qualityResult.countContributingTests());
+    }
+
+    @Test
+    public void testSameTestContributesViaAllThree() {
+        QualityResult qualityResult = new QualityResult(1);
+        qualityResult.setUnitTests(Map.of("id1", "test1"));
+
+        TestFailureInfo failure = new TestFailureInfo("test1", "error");
+        MetaTestReport report = new MetaTestReport(1, 1, 1, List.of(failure));
+        qualityResult.considerMetaTest("meta-test1", report);
+        qualityResult.setCoveragePerTest(Map.of("id1", Set.of(10)));
+        qualityResult.setMutationsKilledPerTest(Map.of("id1", Set.of(1)));
+
+        // still counts as 1 contributing test, not 3
+        assertEquals(1, qualityResult.countContributingTests());
+    }
+
+    @Test
+    public void testMultipleTestsEachContributingDifferently() {
+        QualityResult qualityResult = new QualityResult(3);
+        qualityResult.setUnitTests(Map.of("id1", "test1", "id2", "test2", "id3", "test3"));
+
+        TestFailureInfo failure = new TestFailureInfo("test1", "error");
+        MetaTestReport report = new MetaTestReport(1, 1, 1, List.of(failure));
+        qualityResult.considerMetaTest("meta-test1", report);
+        qualityResult.setCoveragePerTest(Map.of("test2", Set.of(10)));
+        qualityResult.setMutationsKilledPerTest(Map.of("test3", Set.of(1)));
+
+        assertEquals(3, qualityResult.countContributingTests());
+    }
+
+    @Test
+    public void testTestWithEmptySetsDoesNotContribute() {
+        QualityResult qualityResult = new QualityResult(1);
+        qualityResult.setUnitTests(Map.of("id1", "test1"));
+        qualityResult.setCoveragePerTest(Map.of("test1", Set.of()));
+        qualityResult.setMutationsKilledPerTest(Map.of("test1", Set.of()));
+
+        assertEquals(0, qualityResult.countContributingTests());
+    }
 }
